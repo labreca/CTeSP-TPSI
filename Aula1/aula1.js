0b0cda8c069d0d032fee7b72b9f305cd7cefac9e
Maimoce333 <2013212@student.uma.pt>
2018-03-07 21:41:16 +0000

    ex 10

--- Aula1/aula1.js
+++ Aula1/aula1.js
@@ -2,7 +2,7 @@ function notaFinal(notaP1, notaP2, notaPF) {
     return notaFinal = (notaP1 * 0.3 + notaP2 * 0.3 + notaPF * 0.4);
 }
 function main() {
-    
+
     // alinea 5    
     var notaF = notaFinal(12, 13, 14);
     if (notaF < 9.5) {
@@ -12,9 +12,19 @@ function main() {
         console.log("Aprovado");
     }
 
-    // alinea 6
-    mesPorExtenso(13);
 
+    mesPorExtenso(12);
+
+    calculadora(2, 3, "+");
+
+    multiplosDeCinco(5);
+
+    somaPrimeirosCemInteiros(100);
+
+    var fact = factorialDeNumero(5);
+    console.log("O factorial de " + 5 + " é: " + fact);
+
+    // alinea 6
 }
 function mesPorExtenso(mes) {
     var mesPorExtenso
@@ -58,7 +68,61 @@ function mesPorExtenso(mes) {
         default:
             console.log("Valor Inválido!")
     }
-
 }
+
+//alínea 7
+function calculadora(a, b, op) {
+    var resultado
+    if (op == "+") {
+        resultado = a + b;
+    }
+    else if (op == "-") {
+        resultado = a - b;
+    }
+    else if (op == "/") {
+        resultado = a / b;
+    }
+    else if (op == "*") {
+        resultado = a * b;
+    }
+    else if (op == "*") {
+        resultado = Math.pow(a, b);
+    }
+    else {
+        resultado = "nada";
+    }
+    console.log(resultado);
+}
+
+//alinea 8
+function multiplosDeCinco(n) {
+    for (i = 1; i < 20; i++) {
+        if (i % n == 0) {
+            console.log(i);
+        }
+    }
+}
+
+//alinea 9
+function somaPrimeirosCemInteiros (n) {
+    var som = 0;
+    for (i = 1; i <= n; i++) {
+        som = som + i;
+    }
+    console.log(som);
+}
+
+//alinea 10
+function factorialDeNumero (n) {
+    var fact=1;
+    for (i=n; i>0; i--) {
+        fact*=i;
+    }
+    return fact;
+}
+
+
+
+
 
 main();
